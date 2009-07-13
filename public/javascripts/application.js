23fc3c927d224312d770115cf36d89c07ea3dec3
Ben Shymkiw <ben@t.apio.ca>
2009-07-12 22:11:49 -0400

    track external link clicks and open in new window

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+document.observe("dom:loaded", function() {
+  track_external_link_clicks_and_open_in_new_window();
+}
+
+function track_external_link_clicks_and_open_in_new_window() {
+  $$('a').each(function(link) {
+    var url = link.readAttribute('href');
+    if(!url.include(location.host)) {
+      link.writeAttribute('target', '_blank');
+      link.observe('click', record_external_link_click);
+    }
+  });
+}
+
+function record_external_link_click(event) {
+  var url = event.element().readAttribute('href').sub(/^https?\:\/\//i, '');
+  pageTracker._trackPageview('/external/' + url);
+}
