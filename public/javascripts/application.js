07619dee657e610b48b74dbffff5bbcd742e1ca6
Ben Shymkiw <ben@tapiocacollective.com>
2010-06-16 10:06:03 -0400

    rotating interface

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,91 @@
-document.observe("dom:loaded", function() {
-  track_external_link_clicks_and_open_in_new_window();
-}
+var window_width = 1200;
+var window_height = 884;
 
-function track_external_link_clicks_and_open_in_new_window() {
-  $$('a').each(function(link) {
-    var url = link.readAttribute('href');
+jQuery(function($) {
+
+  resizeSections();
+  positionSections();
+
+  // Transition between sections
+  $('#menu a').click(function() {
+    circleTheWagons($(this).attr('href'));
+    return false;
+  });
+  
+  // Open external links in new window and track in Google Analytics
+  $('a').click(function() {
+    var url = $(this).attr('href');
     if(!url.include(location.host)) {
-      link.writeAttribute('target', '_blank');
-      link.observe('click', record_external_link_click);
+      pageTracker._trackPageview('/external/' + url);
+      window.open(url);
+      return false;
     }
+  });
+  
+});
+
+function circleTheWagons(target_id) {
+  var start = $('#main').position();
+  var end = $(target_id).position();
+  if(start.top > 0 && end.top) {
+   if(start.left > end.left) {
+     start_angle = 45;
+     end_angle = -45;
+   } else {
+     start_angle = -45;
+     end_angle = 45;
+   }
+  } else {
+    if(start.left > end.left) {
+     start_angle = 90;
+     end_angle = -90;
+   } else {
+     start_angle = -90;
+     end_angle = 90;
+   }
+  }
+  
+  //caluculateAngles(start_position, end_position);
+  var path = new $.path.bezier({
+    start: {
+      x: start.left,
+      y: start.top,
+      angle: start_angle
+    },
+    end: {
+      x: end.left * -1,
+      y: end.top * -1,
+      angle: end_angle
+    }
+  });
+  $('#main').animate({path: path}, 3000, function() {
+    window.location.replace(target_id);
   });
 }
 
-function record_external_link_click(event) {
-  var url = event.element().readAttribute('href').sub(/^https?\:\/\//i, '');
-  pageTracker._trackPageview('/external/' + url);
+function calculateAngles(start, end) {
+  /*
+  if(start.top == end.top || start.left == end.left) {
+    start_angle = 45;
+    end_angle = -45;
+  }
+  */
+  start_angle = 45;
+  end_angle = -45;
+}
+
+function resizeSections() {
+  window_width = $(window).width();
+  window_height = $(window).height();
+  $('.section').css('width', window_width);
+  $('.section').css('height', window_height);
+}
+
+function positionSections() {
+  $('#main').css('position', 'absolute');
+  $('.section').css('position', 'absolute');
+  $('#work').css('top', (window_height * -1) + 'px').css('left', (window_width * -1) + 'px');
+  $('#about').css('top', (window_height * -1) + 'px').css('left', 0);
+  $('#contact').css('top', 0).css('left', (window_width * -1) + 'px');
+  $('#home').css('top', 0).css('left', 0);
 }
